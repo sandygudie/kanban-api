83b5c0f671f03758b4bd4ec9b41b968199b02d18
sandygudie <goodnewssandy@gmail.com>
2024-07-10 16:18:53 +0100

    Updated Tag routes

--- routes/task.js
+++ routes/task.js
@@ -12,9 +12,8 @@ const {
 const { verifyUser, verifyWorkspaceMember } = require('../middlewares/userChecks')
 
 const taskRouter = Router()
-
-taskRouter.post('/:workspaceId/:columnId', verifyUser, verifyWorkspaceMember, createTask)
 taskRouter.post('/tag/:taskId', verifyUser, createTag)
+taskRouter.post('/:workspaceId/:columnId', verifyUser, verifyWorkspaceMember, createTask)
 taskRouter.patch('/:workspaceId/:columnId/:taskId', verifyUser, verifyWorkspaceMember, updateTask)
 taskRouter.delete('/:workspaceId/:columnId/:taskId', verifyUser, verifyWorkspaceMember, deleteTask)
 taskRouter.get('/:workspaceId/:taskId', verifyUser, verifyWorkspaceMember, getTask)
